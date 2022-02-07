7d6e0e6597068be32ea1f23583a22b5b3c1e77d0
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-07 15:56:17 +0100

    add ternary operator to textColor

--- src/components/TitleComponent.js
+++ src/components/TitleComponent.js
@@ -1,11 +1,11 @@
-const TitleComponent = ({ size, text }) => {
+const TitleComponent = ({ size, text, textColor }) => {
   return (
-    <>
-      <div>
-        <img src="images/favicon.png" alt="Trivial logo" className={size} />
-        <h1 className={size}>{text}</h1>
-      </div>
-    </>
+    <div>
+      <img src="images/favicon.png" alt="Trivial logo" className={size} />
+      <h1 className={`${size} ${textColor === "" ? "black" : "white"}`}>
+        {text}
+      </h1>
+    </div>
   );
 };
 
