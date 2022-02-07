c0ea5d6bd76f3c239d4d3e237b5596ff6d0dfa1b
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-07 15:49:58 +0100

    added style to the RoundedButton component

--- src/components/ButtonComponent/ButtonComponent.js
+++ src/components/ButtonComponent/ButtonComponent.js
@@ -3,7 +3,23 @@ import styled from "styled-components";
 const buttonHeight = 75;
 const buttonWidth = 318;
 
-const RoundedButton = styled.button``;
+const RoundedButton = styled.button`
+  width: ${buttonWidth}px;
+  height: ${buttonHeight}px;
+
+  background-color: #9882ac;
+  color: #f6f7eb;
+
+  border-radius: ${buttonHeight}px;
+  border: none;
+
+  font-size: 36px;
+  font-weight: 600;
+
+  &:hover {
+    background-color: #baacc6;
+  }
+`;
 
 const ButtonComponent = ({ actionOnClick, text }) => {
   return <RoundedButton onClick={actionOnClick}>{text}</RoundedButton>;
