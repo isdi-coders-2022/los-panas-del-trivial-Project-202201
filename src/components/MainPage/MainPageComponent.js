022d027c18d8dd8b2b8290fcac0de1366bfbfe51
Laura Hernández <lhvarela@gmail.com>
2022-02-07 16:22:58 +0100

    Added first code to mainPage component

---
+++ src/components/MainPage/MainPageComponent.js
@@ -0,0 +1,13 @@
+import ButtonComponent from "../ButtonComponent/ButtonComponent";
+import TitleComponent from "../TitleComponent/TitleComponent";
+
+const MainPage = () => {
+  return (
+    <>
+      <TitleComponent></TitleComponent>
+      <ButtonComponent></ButtonComponent>
+    </>
+  );
+};
+
+export default MainPage;
