d2bc001b66478875a71410e1b04b79cc691eaf83
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-07 15:49:28 +0100

    added variables for the button height and width

--- src/components/ButtonComponent/ButtonComponent.js
+++ src/components/ButtonComponent/ButtonComponent.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
+const buttonHeight = 75;
+const buttonWidth = 318;
+
 const RoundedButton = styled.button``;
 
 const ButtonComponent = ({ actionOnClick, text }) => {
