6fa5e1368f3e80bea5204290d4386c92c13efe22
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-08 15:59:11 +0100

    add data-test to BackArrow component

--- src/components/BackArrowComponent/BackArrowComponent.js
+++ src/components/BackArrowComponent/BackArrowComponent.js
@@ -2,13 +2,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
 import { secondary } from "../../globalStyles";
 
-const BackArrow = ({ action }) => {
+const BackArrow = ({ actionOnClick }) => {
   return (
     <FontAwesomeIcon
+      data-testid="arrow"
       icon={faArrowLeftLong}
       size="3x"
       color={secondary}
-      onClick={action}
+      onClick={actionOnClick}
     />
   );
 };
