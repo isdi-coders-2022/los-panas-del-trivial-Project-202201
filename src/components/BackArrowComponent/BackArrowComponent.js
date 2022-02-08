81d952949203dc9b803a15ccebfff8a7f9ac7d07
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-08 10:37:39 +0100

    add secondary color

--- src/components/BackArrowComponent/BackArrowComponent.js
+++ src/components/BackArrowComponent/BackArrowComponent.js
@@ -1,7 +1,8 @@
 import { ArrowCircleLeft } from "react-swm-icon-pack";
+import { secondary } from "../../globalStyles";
 
 const BackArrow = () => {
-  return <ArrowCircleLeft />;
+  return <ArrowCircleLeft color={secondary} />;
 };
 
 export default BackArrow;
