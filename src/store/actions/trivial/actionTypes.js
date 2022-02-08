3493131cf6c661f7b9bf80884de042f65f9f4162
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-08 12:25:15 +0100

    added action names for trivial

--- src/store/actions/trivial/actionTypes.js
+++ src/store/actions/trivial/actionTypes.js
@@ -1,3 +1,9 @@
-const actionTypes = {};
+const actionTypes = {
+  addGame: "add-game",
+  modifyGameNames: "modify-game-names",
+  removeGame: "remove-game",
+  addQuestion: "add-question",
+  removeQuestion: "remove-question",
+};
 
 export default actionTypes;
