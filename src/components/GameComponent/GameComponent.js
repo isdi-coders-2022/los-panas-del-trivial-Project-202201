380fa69dd6d5ecb2eb87e685f4b20c1b4d861a4a
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-08 15:46:13 +0100

    added onClick functions for the gameComponent

--- src/components/GameComponent/GameComponent.js
+++ src/components/GameComponent/GameComponent.js
@@ -3,6 +3,9 @@ import { faPencil, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import { backgroundDark, textPrimary } from "../../globalStyles";
+import { Navigate } from "react-router-dom";
+import { useContext } from "react";
+import TrivialContext from "../../store/contexts/TrivialContext";
 
 const GameContainer = styled.article`
   width: 365px;
@@ -56,7 +59,11 @@ const GameCreator = styled(GameInfoItem)`
   color: #80807e;
 `;
 
-const GameComponent = ({ game: { name, creator, questions, difficulty } }) => {
+const GameComponent = ({
+  game: { id, name, creator, questions, difficulty },
+}) => {
+  const { deleteGame } = useContext(TrivialContext);
+
   return (
     <GameContainer>
       <InfoContiner>
@@ -66,8 +73,16 @@ const GameComponent = ({ game: { name, creator, questions, difficulty } }) => {
         <GameCreator>{creator}</GameCreator>
       </InfoContiner>
       <IconContainer>
-        <FontAwesomeIcon icon={faPencil} />
-        <FontAwesomeIcon icon={faTrashAlt} />
+        <FontAwesomeIcon
+          onClick={Navigate({ to: `game/edit/${id}` })}
+          icon={faPencil}
+        />
+        <FontAwesomeIcon
+          onClick={() => {
+            deleteGame(id);
+          }}
+          icon={faTrashAlt}
+        />
       </IconContainer>
     </GameContainer>
   );
