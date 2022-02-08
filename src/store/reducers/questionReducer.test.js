e450e4a646469efdc9d9e9ed4541e6823decb202
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-08 19:01:23 +0100

    added seccond test for the questionReducer

--- src/store/reducers/questionReducer.test.js
+++ src/store/reducers/questionReducer.test.js
@@ -1,13 +1,34 @@
+import actionTypes from "../actions/trivial/actionTypes";
 import questionReducer from "./questionsReducer";
 
 describe("Given questionReducer", () => {
-  describe("When it's padded an array with 2 questions and the removeQuestion action", () => {
-    test("Then it should return an array with one question", () => {
+  describe("When it's passed an array with 2 questions and the removeQuestion action", () => {
+    test("Then it should return an array without the removed question", () => {
       const questions = [{ id: 3456456456 }, { id: 2 }];
       const expectedQuestions = [{ id: 3456456456 }];
       const action = {
-        type: "remove-question",
+        type: actionTypes.removeQuestion,
         id: 2,
+      };
+
+      const newQuestions = questionReducer(questions, action);
+
+      expect(newQuestions).toEqual(expectedQuestions);
+    });
+  });
+
+  describe("When it's passed an array with 2 questions and the addQuestion action", () => {
+    test("Then it should return an array with the new question", () => {
+      const questions = [{ id: 3456456456 }, { id: 2 }];
+      const expectedQuestions = [
+        { id: 3456456456 },
+        { id: 2 },
+        { id: 3453456 },
+      ];
+      const questionToAdd = { id: 3453456 };
+      const action = {
+        type: actionTypes.addQuestion,
+        question: questionToAdd,
       };
 
       const newQuestions = questionReducer(questions, action);
