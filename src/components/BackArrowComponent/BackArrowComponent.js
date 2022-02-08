79a89ed7142deec1e7697e630bbaeed89b91a3e9
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-08 15:28:55 +0100

    add onclick action in backArrowComponent

--- src/components/BackArrowComponent/BackArrowComponent.js
+++ src/components/BackArrowComponent/BackArrowComponent.js
@@ -2,8 +2,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
 import { secondary } from "../../globalStyles";
 
-const BackArrow = () => {
-  return <FontAwesomeIcon icon={faArrowLeftLong} size="3x" color={secondary} />;
+const BackArrow = ({ action }) => {
+  return (
+    <FontAwesomeIcon
+      icon={faArrowLeftLong}
+      size="3x"
+      color={secondary}
+      onClick={action}
+    />
+  );
 };
 
 export default BackArrow;
