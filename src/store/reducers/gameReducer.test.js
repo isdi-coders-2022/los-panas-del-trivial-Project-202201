05552675cffd69f06f97b7959277789cff1f2a27
Laura Hernández <lhvarela@gmail.com>
2022-02-08 19:08:14 +0100

    test to gameReducer to receive a remove action

--- src/store/reducers/gameReducer.test.js
+++ src/store/reducers/gameReducer.test.js
@@ -1,3 +1,6 @@
+import actionTypes from "../actions/trivial/actionTypes";
+import gameReducer from "./gameReducer";
+
 describe("Given a gameReducer function", () => {
   describe("When it receives a 'remove-game' action, to remove a game with id 2", () => {
     test("Then it should return an array of games without the game with id 2", () => {
@@ -9,7 +12,47 @@ describe("Given a gameReducer function", () => {
           difficulty: "Easy",
           questions: [],
         },
+        {
+          id: 2,
+          name: "Queen Game",
+          creator: "Freddie",
+          difficulty: "Hard",
+          questions: [],
+        },
+        {
+          id: 3,
+          name: "Janis",
+          creator: "Joplin",
+          difficulty: "Easy",
+          questions: [],
+        },
       ];
+
+      const action = {
+        type: actionTypes.removeGame,
+        id: 2,
+      };
+
+      const expectedNewGames = [
+        {
+          id: 1,
+          name: "test's game",
+          creator: "Dan Abramov",
+          difficulty: "Easy",
+          questions: [],
+        },
+        {
+          id: 3,
+          name: "Janis",
+          creator: "Joplin",
+          difficulty: "Easy",
+          questions: [],
+        },
+      ];
+
+      const newGameArray = gameReducer(currentGames, action);
+
+      expect(newGameArray).toEqual(expectedNewGames);
     });
   });
 });
