8818e363584d35d75dab48d7e801b65cba0df9b7
Laura Hernández <lhvarela@gmail.com>
2022-02-08 10:53:33 +0100

    added test to alternative text in image

--- src/components/TitleComponent/TitleComponent.test.js
+++ src/components/TitleComponent/TitleComponent.test.js
@@ -15,4 +15,31 @@ describe("Given a TitleComponent", () => {
       expect(foundTitle).toBeInTheDocument();
     });
   });
+  describe("When it receives font-size 'medium'", () => {
+    test("Then it should render a title with font-size 36px", () => {
+      const size = "medium";
+      const text = "ReactIsAwesome";
+
+      render(<TitleComponent size={size} text={text} textColor={""} />);
+
+      const foundTitle = screen.queryByText(text);
+      const expectedSize = "36px";
+
+      expect(foundTitle.style.fontSize).toMatch(expectedSize);
+    });
+  });
+  describe("When it's instantiated", () => {
+    test("Then it should render a title with an image logo with alternative text 'Trivial logo'", () => {
+      const text = "Magic";
+      const altText = "Trivial logo";
+
+      render(
+        <TitleComponent size={"medium"} text={text} textColor={"black"} />
+      );
+
+      const foundAltText = screen.queryByRole("img", { alt: altText });
+
+      expect(foundAltText).toBeInTheDocument();
+    });
+  });
 });
