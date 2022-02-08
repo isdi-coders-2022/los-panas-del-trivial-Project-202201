9dda9b6e297868e0251be16b5d2fb2d3ce5ead54
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-08 15:58:48 +0100

    add describes to backArrow tests

---
+++ src/components/BackArrowComponent/BackArrowComponent.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import BackArrow from "./BackArrowComponent";
+
+describe("Given BackArrowComponent", () => {
+  describe("When it recieves an actionOnClick", () => {
+    test("Then actionOnClick should be invoked when the button is clicked", () => {});
+  });
+});
