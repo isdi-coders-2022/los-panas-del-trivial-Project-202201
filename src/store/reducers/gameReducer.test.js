0d3af212b217ebca445eafe84369a674e65b8608
Laura Hernández <lhvarela@gmail.com>
2022-02-08 18:48:53 +0100

    initial test to gameReducer

---
+++ src/store/reducers/gameReducer.test.js
@@ -0,0 +1,15 @@
+describe("Given a gameReducer function", () => {
+  describe("When it receives a 'remove-game' action, to remove a game with id 2", () => {
+    test("Then it should return an array of games without the game with id 2", () => {
+      const currentGames = [
+        {
+          id: 1,
+          name: "test's game",
+          creator: "Dan Abramov",
+          difficulty: "Easy",
+          questions: [],
+        },
+      ];
+    });
+  });
+});
