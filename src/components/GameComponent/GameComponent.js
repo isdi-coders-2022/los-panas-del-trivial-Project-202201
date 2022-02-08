caf912819c674c2b6c1559f1d82bb3b7e4f4e17c
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-08 10:05:38 +0100

    added html for the GameComponent

--- src/components/GameComponent/GameComponent.js
+++ src/components/GameComponent/GameComponent.js
@@ -1,3 +1,15 @@
-const GameComponent = ({ game }) => {};
+const GameComponent = ({ game }) => {
+  return (
+    <article>
+      <div>
+        <h2>Game name</h2>
+        <p>{`Difficulty: ${game.difficulty}`}</p>
+        <p>{`Questions: ${game.questrions.length}`}</p>
+        <p>{game.creator}</p>
+      </div>
+      <div></div>
+    </article>
+  );
+};
 
 export default GameComponent;
