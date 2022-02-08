7210313c1034d68fd71dcdeef269665dd8e6897b
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-08 12:03:29 +0100

    add proptypes to backArrow

--- src/components/BackArrowComponent/BackArrowComponent.js
+++ src/components/BackArrowComponent/BackArrowComponent.js
@@ -1,8 +1,13 @@
 import { ArrowCircleLeft } from "react-swm-icon-pack";
 import { secondary } from "../../globalStyles";
+import PropTypes from "prop-types";
 
 const BackArrow = ({ size }) => {
   return <ArrowCircleLeft color={secondary} strokeWidth={"2px"} size={size} />;
+};
+
+BackArrow.propTypes = {
+  size: PropTypes.oneOf(["70", "50"]).isRequired,
 };
 
 export default BackArrow;
