623c0f03c4b032e496769b4e9c544b98c92604c4
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-08 19:09:55 +0100

    add setError test

--- src/store/reducers/apiReducer.test.js
+++ src/store/reducers/apiReducer.test.js
@@ -15,4 +15,19 @@ describe("Given a apiReducer reducer", () => {
       expect(newStatus).toEqual(expectedOutput);
     });
   });
+
+  describe("When it receives true and setError action", () => {
+    test("Then it should return error: true", () => {
+      const apiStatus = { error: true };
+      const action = {
+        type: "set-error",
+        mode: true,
+      };
+      const expectedOutput = { error: true };
+
+      const newStatus = apiReducer(apiStatus, action);
+
+      expect(newStatus).toEqual(expectedOutput);
+    });
+  });
 });
