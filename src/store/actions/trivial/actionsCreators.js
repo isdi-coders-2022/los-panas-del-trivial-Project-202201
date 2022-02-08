bd23b4387152872974d11ee28da7ab0296e8f538
Laura Hernández <lhvarela@gmail.com>
2022-02-08 12:58:11 +0100

    added params to actionCreators

--- src/store/actions/trivial/actionsCreators.js
+++ src/store/actions/trivial/actionsCreators.js
@@ -1,21 +1,26 @@
 import actionTypes from "./actionTypes";
 
-export const addGameAction = () => ({
+export const addGameAction = (game) => ({
   type: actionTypes.addGame,
+  game: game,
 });
 
-export const modifyGameNamesAction = () => ({
+export const modifyGameNamesAction = (gameNames) => ({
   type: actionTypes.modifyGameNames,
+  gameNames: gameNames,
 });
 
-export const removeGameAction = () => ({
+export const removeGameAction = (id) => ({
   type: actionTypes.removeGame,
+  id: id,
 });
 
-export const addQuestionAction = () => ({
+export const addQuestionAction = (id) => ({
   type: actionTypes.addQuestion,
+  id: id,
 });
 
-export const removeQuestions = () => ({
+export const removeQuestions = (id) => ({
   type: actionTypes.removeQuestion,
+  id: id,
 });
