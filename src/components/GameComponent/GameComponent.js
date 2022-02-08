0f6c0fbb96687ea0c1f488849ac9133ea1ce711c
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-08 10:36:25 +0100

    added html and icons for GameComponent

--- src/components/GameComponent/GameComponent.js
+++ src/components/GameComponent/GameComponent.js
@@ -1,13 +1,19 @@
-const GameComponent = ({ game }) => {
+import { SWMIcon } from "react-swm-icon-pack";
+import { textPrimary } from "../../globalStyles";
+
+const GameComponent = ({ game: { name, creator, questions, difficulty } }) => {
   return (
     <article>
       <div>
-        <h2>Game name</h2>
-        <p>{`Difficulty: ${game.difficulty}`}</p>
-        <p>{`Questions: ${game.questrions.length}`}</p>
-        <p>{game.creator}</p>
+        <h2>{name}</h2>
+        <p>{`Difficulty: ${difficulty}`}</p>
+        <p>{`Questions: ${questions.length}`}</p>
+        <p>{creator}</p>
       </div>
-      <div></div>
+      <div>
+        <SWMIcon name="Edit1" color={textPrimary} set="outline" />
+        <SWMIcon name="CrossCircle" color={textPrimary} set="outline" />;
+      </div>
     </article>
   );
 };
