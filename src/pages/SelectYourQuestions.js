7e64f8187278223acddcd2ea4d6ad44fa6a4ff31
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-08 20:38:24 +0100

    add select-your-questions header

--- src/pages/SelectYourQuestions.js
+++ src/pages/SelectYourQuestions.js
@@ -1,1 +1,51 @@
-const SelectYourQuestions = () => {};
+import styled from "styled-components";
+import BackArrow from "../components/BackArrowComponent/BackArrowComponent";
+import TitleComponent from "../components/TitleComponent/TitleComponent";
+import { backgroundLight, secondary } from "../globalStyles";
+
+const PageContainer = styled.div`
+  background-color: ${backgroundLight};
+  height: 100vh;
+  padding: 36px 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ArrowContainer = styled.div`
+  position: absolute;
+  left: 20px;
+`;
+
+const TitleContainer = styled.div`
+  position: relative;
+  left: 20px;
+`;
+
+const SelectYourQuestions = () => {
+  return (
+    <>
+      <PageContainer>
+        <HeaderContainer>
+          <ArrowContainer>
+            <BackArrow />
+          </ArrowContainer>
+          <TitleContainer>
+            <TitleComponent
+              size="small"
+              textColor={secondary}
+              text="Select Your Questions"
+            />
+          </TitleContainer>
+        </HeaderContainer>
+      </PageContainer>
+    </>
+  );
+};
+
+export default SelectYourQuestions;
