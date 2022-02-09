532955dada4ae2bb9167dbe2e387c71dd844b638
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-09 12:27:30 +0100

    added mock game

--- src/pages/GameListPage.js
+++ src/pages/GameListPage.js
@@ -1,16 +1,45 @@
 import styled from "styled-components";
 import BackArrowComponent from "../components/BackArrowComponent/BackArrowComponent";
 import TitleComponent from "../components/TitleComponent/TitleComponent";
+import GameComponent from "../components/GameComponent/GameComponent";
+import { backgroundLight } from "../globalStyles";
 
-const GameList = styled.ul``;
+const PageContainer = styled.div`
+  background-color: ${backgroundLight};
+  padding: 36px 20px;
+  height: 100vh;
+`;
+
+const GameList = styled.ul`
+  list-style: none;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 25px;
+`;
+
+const GameHolder = styled.li``;
 
 const GameListPage = () => {
   return (
-    <div>
+    <PageContainer>
       <BackArrowComponent actionOnClick={() => {}} />
       <TitleComponent size="medium" text="Game List" />
-      <GameList></GameList>
-    </div>
+      <GameList>
+        <GameHolder>
+          <GameComponent
+            game={{
+              id: 3,
+              name: "",
+              creator: "",
+              difficulty: "",
+              questions: [],
+            }}
+          ></GameComponent>
+        </GameHolder>
+      </GameList>
+    </PageContainer>
   );
 };
 
