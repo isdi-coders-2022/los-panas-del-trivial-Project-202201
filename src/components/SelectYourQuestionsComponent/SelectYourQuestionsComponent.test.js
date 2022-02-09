6600e6a5e54b4cf14ec1813875171557d870943c
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-09 11:38:05 +0100

    add title text rendered test

--- src/components/SelectYourQuestionsComponent/SelectYourQuestionsComponent.test.js
+++ src/components/SelectYourQuestionsComponent/SelectYourQuestionsComponent.test.js
@@ -1,5 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import SelectYourQuestionsComponent from "./SelectYourQuestionsComponent";
+
 describe("Given a SelectYourQuestionsComponent", () => {
   describe("When it's instantiated", () => {
-    test("Then it should render a heading with the text 'Select Your Questions'", () => {});
+    test("Then it should render a heading with the text 'Select Your Questions'", () => {
+      const expectedText = "Select Your Questions";
+
+      render(<SelectYourQuestionsComponent />);
+
+      const foundHeading = screen.queryByRole("heading");
+
+      expect(foundHeading.textContent).toBe(expectedText);
+    });
   });
 });
