300ed27f99de15fe8a04824d2a02a04460d810ed
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-09 14:06:26 +0100

    add forEach category fetch

--- src/hooks/useAPI.js
+++ src/hooks/useAPI.js
@@ -7,6 +7,16 @@ const useAPI = () => {
 
   const gamesAPIurl = "https://trivial-provider.herokuapp.com/games";
   const { gamesDispatch, questionDispatch } = useContext(TrivialContext);
+
+  const loadQuestionsAPI = useCallback(async (difficulty) => {
+    const idCategory = [21, 27, 15, 18, 22];
+    let allQuestions = [];
+    idCategory.forEach(async (category) => {
+      const response = await fetch(getTrivialUrl(category, difficulty));
+      const questions = await response.json().results;
+      allQuestions.push(questions);
+    });
+  });
 };
 
 export default useAPI;
