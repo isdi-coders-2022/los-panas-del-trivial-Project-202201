acec96ade2e6171459828e07f17486b15650666d
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-09 10:19:57 +0100

    added backarrow and title compoonent to game list

--- src/pages/GameListPage.js
+++ src/pages/GameListPage.js
@@ -1,3 +1,13 @@
-const GameListPage = () => {};
+import BackArrowComponent from "../components/BackArrowComponent/BackArrowComponent";
+import TitleComponent from "../components/TitleComponent/TitleComponent";
+
+const GameListPage = () => {
+  return (
+    <div>
+      <BackArrowComponent actionOnClick={() => {}} />
+      <TitleComponent size="medium" text="Game List" />
+    </div>
+  );
+};
 
 export default GameListPage;
