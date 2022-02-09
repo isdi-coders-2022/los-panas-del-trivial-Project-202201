9f77c7a489d4344b8ea457819ee0bcbec0730c59
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-09 12:19:12 +0100

    changed ul to GameList component

--- src/pages/GameListPage.js
+++ src/pages/GameListPage.js
@@ -1,12 +1,15 @@
+import styled from "styled-components";
 import BackArrowComponent from "../components/BackArrowComponent/BackArrowComponent";
 import TitleComponent from "../components/TitleComponent/TitleComponent";
+
+const GameList = styled.ul``;
 
 const GameListPage = () => {
   return (
     <div>
       <BackArrowComponent actionOnClick={() => {}} />
       <TitleComponent size="medium" text="Game List" />
-      <ul></ul>
+      <GameList></GameList>
     </div>
   );
 };
