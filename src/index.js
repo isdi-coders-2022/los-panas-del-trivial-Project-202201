13c8d99d06a3d513272a556f67cde87cbf9a5583
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-09 12:31:18 +0100

    added the context providers to the index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import APIContextProvider from "./store/contexts/APIContextProvider";
+import TrivialContextProvider from "./store/contexts/TrivialContextProvider";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <APIContextProvider>
+        <TrivialContextProvider>
+          <App />
+        </TrivialContextProvider>
+      </APIContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
