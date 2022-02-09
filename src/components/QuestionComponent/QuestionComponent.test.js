c7c0318cd4915fb63c5e341abe644048c7f36336
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-09 21:19:03 +0100

    add questionText test

--- src/components/QuestionComponent/QuestionComponent.test.js
+++ src/components/QuestionComponent/QuestionComponent.test.js
@@ -1,5 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import QuestionComponent from "./QuestionComponent";
+
 describe("Given a QuestionComponent", () => {
-  describe("When it's instantiated", () => {
-    test("Then it should render two texts", () => {});
+  describe("When it's instantiated with a questionText = 'hola", () => {
+    test("Then it should render a questionText", () => {
+      const expectedText = "hola";
+      const typeQuestionText = "asds";
+
+      render(
+        <QuestionComponent
+          questionText={expectedText}
+          typeQuestionText={typeQuestionText}
+        />
+      );
+
+      const findText = screen.queryAllByText(expectedText);
+
+      expect(findText[0]).toBeInTheDocument();
+    });
   });
 });
