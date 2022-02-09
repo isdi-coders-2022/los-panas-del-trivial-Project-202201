317e57c573f06f2665aa62777d2d43ba30df6cb8
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-09 12:34:46 +0100

    add colors props to isSelected estate

--- src/components/QuestionComponent/QuestionComponent.js
+++ src/components/QuestionComponent/QuestionComponent.js
@@ -1,13 +1,23 @@
 import styled from "styled-components";
+import { backgroundDark, primary, textPrimary } from "../../globalStyles";
 
 const CardContainer = styled.div`
-  background-color: ${(props) => props.color};
+  background-color: ${(props) => props.backgroundColor};
   min-height: 77px;
   width: 365px;
   border-radius: 9px;
 `;
 
 const QuestionComponent = ({ isSelected }) => {
+  let backgroundColor = `${backgroundDark}`;
+  let questionTextColor = `${textPrimary}`;
+  let typeTextQuestionColor = "#fff";
+
+  if (isSelected) {
+    backgroundColor = `${primary}`;
+    questionTextColor = `${backgroundDark}`;
+  }
+
   return (
     <>
       <CardContainer isSelected={isSelected}></CardContainer>
