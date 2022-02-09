9f576a6dc70d15b0ce183e0ddf20a5ff3b9aaac2
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-09 23:46:31 +0100

    added second test

--- src/pages/NotFoundPage.test.js
+++ src/pages/NotFoundPage.test.js
@@ -3,12 +3,22 @@ import NotFoundPage from "./NotFoundPage";
 
 describe("Given NotFoundPage", () => {
   describe("When it's rendered", () => {
-    test("Then it should present a heading with the text 'PageNotFound'", () => {
+    test("Then it should present a h1 element with the text 'PageNotFound'", () => {
       const expectedText = "Page Not Found";
 
       render(<NotFoundPage />);
 
       const headingElement = screen.queryByRole("heading", { level: 1 });
+
+      expect(headingElement.textContent).toBe(expectedText);
+    });
+
+    test("Then it should present a h2 element with the text '404'", () => {
+      const expectedText = "404";
+
+      render(<NotFoundPage />);
+
+      const headingElement = screen.queryByRole("heading", { level: 2 });
 
       expect(headingElement.textContent).toBe(expectedText);
     });
