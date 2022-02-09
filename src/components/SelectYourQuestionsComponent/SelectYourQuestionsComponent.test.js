0d8ace0764899adc0c460d160373ffb9ea623427
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-09 11:35:34 +0100

    add SelectYourQuestions test

---
+++ src/components/SelectYourQuestionsComponent/SelectYourQuestionsComponent.test.js
@@ -0,0 +1,5 @@
+describe("Given a SelectYourQuestionsComponent", () => {
+  describe("When it's instantiated", () => {
+    test("Then it should render a heading with the text 'Select Your Questions'", () => {});
+  });
+});
