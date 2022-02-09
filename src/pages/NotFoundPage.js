5d52ada742c06fb51409bce2d2acafab23aa87b3
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-09 23:31:41 +0100

    added 404 status code

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,12 +1,33 @@
 import styled from "styled-components";
-import { backgroundLight } from "../globalStyles";
+import { backgroundLight, secondary } from "../globalStyles";
 
 const PageHolder = styled.div`
   background-color: ${backgroundLight};
   height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Status = styled.h2`
+  font-size: 70px;
+  margin: 0;
+  color: ${secondary};
+`;
+const Info = styled.h1`
+  margin: 0;
+  color: ${secondary};
+  margin-bottom: 300px;
 `;
 
 const NotFoundPage = () => {
-  return <PageHolder></PageHolder>;
+  return (
+    <PageHolder>
+      <Status>404</Status>
+      <Info>Page Not Found</Info>
+    </PageHolder>
+  );
 };
 export default NotFoundPage;
