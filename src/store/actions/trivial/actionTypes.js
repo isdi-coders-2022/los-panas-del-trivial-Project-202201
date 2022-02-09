f6b222a6fbf770140dfb89ba0e793580e1c1a920
Laura Hernández <lhvarela@gmail.com>
2022-02-09 17:48:20 +0100

    added loadGames to actiontypes

--- src/store/actions/trivial/actionTypes.js
+++ src/store/actions/trivial/actionTypes.js
@@ -2,6 +2,7 @@ const actionTypes = {
   addGame: "add-game",
   modifyGameNames: "modify-game-names",
   removeGame: "remove-game",
+  loadGames: "load-games",
   addQuestion: "add-question",
   removeQuestion: "remove-question",
   loadQuestions: "load-all-questions",
