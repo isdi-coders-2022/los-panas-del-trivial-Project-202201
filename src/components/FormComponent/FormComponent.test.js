faf8dd0b51e5b6e95d53fb4c9f7c3c30914965e3
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-09 10:36:18 +0100

    added seccond test for the form

--- src/components/FormComponent/FormComponent.test.js
+++ src/components/FormComponent/FormComponent.test.js
@@ -1,21 +1,36 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import FormComponent from "./FormComponent";
 
 describe("Given FormComponent", () => {
   describe("When it is rendered", () => {
-    test("Then it should render 2 inputs and 1 select with 3 options", () => {
+    test("Then it should render 2 inputs, 1 select with 3 options and a button", () => {
       const expectedOptions = 3;
 
       render(<FormComponent onSubmit={() => {}} />);
 
       const creatorInput = screen.queryByPlaceholderText("Input your name");
       const nameInput = screen.queryByPlaceholderText("Input the game's name");
-
+      const submitButton = screen.queryByRole("button");
       const selectsDifficulty = screen.queryAllByRole("option");
 
       expect(creatorInput).toBeInTheDocument();
       expect(nameInput).toBeInTheDocument();
+      expect(submitButton).toBeInTheDocument();
       expect(selectsDifficulty.length).toBe(expectedOptions);
+    });
+  });
+
+  describe("When it is instanciated passing an onSubmit action", () => {
+    test("Then it should call the onSubmit action when the button is pressed", () => {
+      const action = jest.fn();
+
+      render(<FormComponent onSubmit={action} />);
+
+      const submitButton = screen.queryByRole("button");
+
+      userEvent.click(submitButton);
+      expect(action).toHaveBeenCalled();
     });
   });
 });
