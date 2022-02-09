5ed021c424cf5397c030a14b5481e2e5452b991e
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-09 21:29:15 +0100

    set test texts

--- src/components/QuestionComponent/QuestionComponent.test.js
+++ src/components/QuestionComponent/QuestionComponent.test.js
@@ -3,7 +3,7 @@ import QuestionComponent from "./QuestionComponent";
 
 describe("Given a QuestionComponent", () => {
   describe("When it's instantiated with a questionText = 'hola", () => {
-    test("Then it should render a questionText", () => {
+    test("Then it should render a questionText text", () => {
       const expectedText = "hola";
       const typeQuestionText = "nohola";
 
@@ -20,7 +20,7 @@ describe("Given a QuestionComponent", () => {
     });
   });
   describe("When it's instantiated with a typeQuestionText = 'hola", () => {
-    test("Then it should render a questionText", () => {
+    test("Then it should render a questionText text", () => {
       const expectedText = "hola";
       const questionText = "nohola";
 
