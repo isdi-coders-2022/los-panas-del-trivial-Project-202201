42cf214aa3c20da59461f175343d386b2ac4a138
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-09 12:24:49 +0100

    add CardContainer

--- src/components/QuestionComponent/QuestionComponent.js
+++ src/components/QuestionComponent/QuestionComponent.js
@@ -1,7 +1,16 @@
-const QuestionComponent = () => {
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  background-color: ${(props) => props.color};
+  min-height: 77px;
+  width: 365px;
+  border-radius: 9px;
+`;
+
+const QuestionComponent = ({ isSelected }) => {
   return (
     <>
-      <p>hola</p>
+      <CardContainer></CardContainer>
     </>
   );
 };
