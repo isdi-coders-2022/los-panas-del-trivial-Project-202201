a467083007fdbecd871034f5bea9d7066af68f32
Laura Hernández <lhvarela@gmail.com>
2022-02-10 12:21:50 +0100

    added test to toggle questions selected-unselected

--- src/store/reducers/allQuestionsReducer.test.js
+++ src/store/reducers/allQuestionsReducer.test.js
@@ -45,4 +45,50 @@ describe("Given a allQuestionsReducer reducer", () => {
       expect(newAllQuestions).toEqual(expectedQuestions);
     });
   });
+  describe("When it receives an array with two unselected questions and an action to selected the first one", () => {
+    test("Then it should return an array with one selected question and one unselected question", () => {
+      const currentQuestions = [
+        {
+          question: "text some question",
+          type: "",
+          selected: false,
+          id: 0,
+          category: "",
+        },
+        {
+          question: "another question",
+          type: "",
+          selected: false,
+          id: 1,
+          category: "",
+        },
+      ];
+
+      const action = {
+        type: "toggle-selected-question",
+        id: 0,
+      };
+
+      const expectedQuestions = [
+        {
+          question: "text some question",
+          type: "",
+          selected: true,
+          id: 0,
+          category: "",
+        },
+        {
+          question: "another question",
+          type: "",
+          selected: false,
+          id: 1,
+          category: "",
+        },
+      ];
+
+      const newAllQuestions = allQuestionsReducer(currentQuestions, action);
+
+      expect(newAllQuestions).toEqual(expectedQuestions);
+    });
+  });
 });
