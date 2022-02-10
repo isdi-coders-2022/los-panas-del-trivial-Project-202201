79e49f461c1b4dd23f54859b4bfbecf58994f5f6
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-10 22:50:40 +0100

    delete apicontext from index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import APIContextProvider from "./store/contexts/APIContextProvider";
 import TrivialContextProvider from "./store/contexts/TrivialContextProvider";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <APIContextProvider>
-        <TrivialContextProvider>
-          <App />
-        </TrivialContextProvider>
-      </APIContextProvider>
+      <TrivialContextProvider>
+        <App />
+      </TrivialContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
