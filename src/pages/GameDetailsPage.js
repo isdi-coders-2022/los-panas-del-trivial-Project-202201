2ded7984700a2dacf1fadff136c84fe1ef6e867f
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-10 19:38:37 +0100

    added logic to display game and questions

--- src/pages/GameDetailsPage.js
+++ src/pages/GameDetailsPage.js
@@ -1,5 +1,19 @@
-const GameDetailsPage = ({ game }) => {
-  return <div>sdwafsaf</div>;
+import GameComponent from "../components/GameComponent/GameComponent";
+import QuestionComponent from "../components/QuestionComponent/QuestionComponent";
+
+const GameDetailsPage = () => {
+  const game = { name: "a", creator: "b", questions: [], difficulty: "medium" };
+  return (
+    <div>
+      <h2>Game details</h2>
+      <GameComponent game={game} />
+      <ul>
+        {game.questions.map((question) => (
+          <QuestionComponent question={question} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default GameDetailsPage;
