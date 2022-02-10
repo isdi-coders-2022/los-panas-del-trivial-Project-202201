e34df40b0a7c7813edd3be112834f86a9c39675d
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-10 22:15:33 +0100

    add describers tot test

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+describe("Given a App component", () => {
+  describe("When it's instantiated", () => {
+    test("Then it should render a header with Trivial.Provider text", () => {});
+  });
+});
