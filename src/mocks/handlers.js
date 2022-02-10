fea75b1de523278b55d70f74106525b127093b11
Laura Hernández <lhvarela@gmail.com>
2022-02-10 20:14:52 +0100

    added env file to gitignore

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,7 +1,7 @@
 import { rest } from "msw";
 
 export const handlers = [
-  rest.get("https://trivial-provider.herokuapp.com/games/", (req, res, ctx) => {
+  rest.get("process.env.REACT_APP_API_HEROKU_URL", (req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json([
