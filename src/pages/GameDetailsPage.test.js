b86dc956c1e11d753f389cf75432acc01683d733
Laura Hernández <lhvarela@gmail.com>
2022-02-10 22:24:42 +0100

    test to gameDetails Page

---
+++ src/pages/GameDetailsPage.test.js
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import TrivialContext from "../store/contexts/TrivialContext";
+import GameDetailsPage from "./GameDetailsPage";
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: () => ({ id: "3" }),
+}));
+
+describe("Given a GameDetailsPage component", () => {
+  describe("When it receives a game with 'Dan Abramov' as creator name", () => {
+    test("Then it should render a game detail with the text 'Dan Abramov'", () => {
+      const expectedText = "Dan Abramov";
+
+      const currentGames = [
+        {
+          id: 3,
+          name: "test's game",
+          creator: "Dan Abramov",
+          difficulty: "Easy",
+          questions: [],
+        },
+      ];
+
+      render(
+        <BrowserRouter>
+          <TrivialContext.Provider value={{ currentGames }}>
+            <GameDetailsPage />
+          </TrivialContext.Provider>
+        </BrowserRouter>
+      );
+
+      const creatorElement = screen.getByText(expectedText);
+
+      expect(creatorElement).toBeInTheDocument();
+    });
+  });
+});
