25b65a5288bd8a934aafe68f1986995ddd855eba
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-10 22:15:48 +0100

    add app test render the title

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,16 @@ import App from "./App";
 
 describe("Given a App component", () => {
   describe("When it's instantiated", () => {
-    test("Then it should render a header with Trivial.Provider text", () => {});
+    test("Then it should render a header with Trivial.Provider text", () => {
+      const text = "Trivial.Provider";
+      render(
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      );
+
+      const foundText = screen.queryByText(text);
+      expect(foundText).toBeInTheDocument();
+    });
   });
 });
