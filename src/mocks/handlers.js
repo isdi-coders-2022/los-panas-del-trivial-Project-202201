78b3c76081f020bce515cbaababb3056421dcd1c
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-10 21:42:09 +0100

    add object to post in handlers

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -24,6 +24,17 @@ export const handlers = [
   }),
 
   rest.post(`${process.env.REACT_APP_API_HEROKU_URL}`, (req, res, ctx) => {
-    return res(ctx.status(201), ctx.json([]));
+    return res(
+      ctx.status(201),
+      ctx.json([
+        {
+          name: "pruebamsw",
+          creator: "sdfg",
+          difficulty: "easy",
+          questions: [],
+          id: 1,
+        },
+      ])
+    );
   }),
 ];
