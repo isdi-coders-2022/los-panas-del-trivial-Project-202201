720370bf0cfa1bfa9041fc5087aa4322cfd5359c
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-12 12:22:34 +0100

    any category display test

--- src/components/FilterComponent/FilterComponent.test.js
+++ src/components/FilterComponent/FilterComponent.test.js
@@ -1,5 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import FilterComponent from "./FilterComponent";
+
 describe("Given a FilterComponent", () => {
   describe("When it's rendered", () => {
-    test("Then, it should display Any Category option");
+    test("Then, it should display Any Category option", () => {
+      const value = "Any Category";
+
+      render(
+        <FilterComponent data={("Type", () => {}, "category", () => {})} />
+      );
+
+      const foundText = screen.getByText(value);
+
+      expect(foundText).toBeInTheDocument();
+    });
   });
 });
