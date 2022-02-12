50c35b9fda351d6c392dc80406680747d87a1908
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-12 13:30:46 +0100

    added base for editing page

---
+++ src/pages/EditGamePage.js
@@ -0,0 +1,60 @@
+import BackArrowComponent from "../components/BackArrowComponent/BackArrowComponent";
+import TitleComponent from "../components/TitleComponent/TitleComponent";
+import styled from "styled-components";
+import { backgroundLight } from "../globalStyles";
+import FormComponent from "../components/FormComponent/FormComponent";
+import { useContext, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import TrivialContext from "../store/contexts/TrivialContext";
+
+const PageContainer = styled.div`
+  background-color: ${backgroundLight};
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 80px;
+`;
+
+const BackArrowContainer = styled.div`
+  padding: 36px 20px;
+  width: 100%;
+`;
+
+const EditGamePage = () => {
+  const params = useParams();
+  const { currentGames } = useContext(TrivialContext);
+  const navigate = useNavigate();
+  const game = currentGames.find(
+    (currentGame) => currentGame.id === +params.id
+  );
+
+  const [name, setName] = useState(game.name);
+  const [creator, setCreator] = useState(game.creator);
+  const [difficulty, setDifficulty] = useState(game.difficulty);
+
+  const actionOnSubmit = () => {};
+
+  const gotoGamesList = () => {
+    navigate(`/games-list`);
+  };
+
+  return (
+    <PageContainer>
+      <BackArrowContainer>
+        <BackArrowComponent actionOnClick={gotoGamesList} />
+      </BackArrowContainer>
+      <TitleComponent text={"Edit Game"} size={"medium"}></TitleComponent>
+      <FormComponent
+        editing
+        name={{ name, setName }}
+        creator={{ creator, setCreator }}
+        difficulty={{ difficulty, setDifficulty }}
+        onSubmit={actionOnSubmit}
+      ></FormComponent>
+    </PageContainer>
+  );
+};
+
+export default EditGamePage;
