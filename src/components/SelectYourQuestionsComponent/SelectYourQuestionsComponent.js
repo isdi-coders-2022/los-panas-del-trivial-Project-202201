47ab926205fb9ea14d563368d8606246b8dcbe3e
Laura Hernández <lhvarela@gmail.com>
2022-02-12 18:59:51 +0100

    added paginateAllQuestions function to divide the array in 20questions

--- src/components/SelectYourQuestionsComponent/SelectYourQuestionsComponent.js
+++ src/components/SelectYourQuestionsComponent/SelectYourQuestionsComponent.js
@@ -102,6 +102,15 @@ const SelectYourQuestionsComponent = ({ onSave }) => {
     arrayToRender = [...currentAllQuestions];
   }
 
+  const paginateAllQuestions = () => {
+    const arrayToRenderPaginated = [...arrayToRender];
+    const questionsPage1 = arrayToRenderPaginated.slice(0, 20);
+    const questionsPage2 = arrayToRenderPaginated.slice(20, 40);
+    const questionsPage3 = arrayToRenderPaginated.slice(40, 60);
+    const questionsPage4 = arrayToRenderPaginated.slice(60, 80);
+    const questionsPage5 = arrayToRenderPaginated.slice(80, 100);
+  };
+
   const gotoMainPage = () => {
     allQuestionsDispatch(emptyQuestionsAction());
     questionDispatch(emptyQuestionsAction());
@@ -140,6 +149,7 @@ const SelectYourQuestionsComponent = ({ onSave }) => {
             />
           ))}
         </MainContainer>
+
         <FooterContainer>
           <TotalSelectedQuestons>{`${currentQuestions.length} selected questions`}</TotalSelectedQuestons>
           <ButtonComponent text="Save" actionOnClick={onSave} />
