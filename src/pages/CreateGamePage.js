16da9d71632610ad0b300e68386b5d6b65dc9e40
Laura Hernández <lhvarela@gmail.com>
2022-02-13 19:15:34 +0100

    added title in left container

--- src/pages/CreateGamePage.js
+++ src/pages/CreateGamePage.js
@@ -1,7 +1,13 @@
 import BackArrowComponent from "../components/BackArrowComponent/BackArrowComponent";
 import TitleComponent from "../components/TitleComponent/TitleComponent";
 import styled from "styled-components";
-import { backgroundLight } from "../globalStyles";
+import {
+  backgroundDark,
+  backgroundLight,
+  breakpointM,
+  breakpointS,
+  secondary,
+} from "../globalStyles";
 import FormComponent from "../components/FormComponent/FormComponent";
 import useAPI from "../hooks/useAPI";
 import { useContext, useState } from "react";
@@ -18,6 +24,42 @@ const PageContainer = styled.div`
   align-items: center;
   justify-content: space-between;
   padding-bottom: 80px;
+  @media (min-width: ${breakpointS}) {
+    background-color: ${secondary};
+  }
+`;
+
+const TitleContainer = styled.div`
+  height: 30vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media (min-width: ${breakpointS}) {
+    position: absolute;
+    left: 3%;
+  }
+  @media (min-width: ${breakpointM}) {
+    position: absolute;
+    left: 8%;
+  }
+`;
+const ContainerGameDark = styled.div`
+  background-color: ${backgroundLight};
+  border-radius: 50px 50px 0 0;
+  width: 100%;
+  position: absolute;
+  bottom: 0px;
+  height: 70%;
+  @media (min-width: ${breakpointS}) {
+    border-radius: 50px 0 0 50px;
+    position: absolute;
+    height: 100%;
+    width: 60%;
+    right: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+  }
 `;
 
 const BackArrowContainer = styled.div`
@@ -78,16 +120,28 @@ const CreateGamePage = () => {
     if (viewCreateGamePage) {
       return (
         <PageContainer>
-          <BackArrowContainer>
-            <BackArrowComponent actionOnClick={gotoMainPage} />
-          </BackArrowContainer>
-          <TitleComponent text={"Create Game"} size={"medium"}></TitleComponent>
-          <FormComponent
-            name={{ name, setName }}
-            creator={{ creator, setCreator }}
-            difficulty={{ difficulty, setDifficulty }}
-            onSubmit={actionOnSubmit}
-          ></FormComponent>
+          <TitleContainer>
+            <TitleComponent
+              text={"Create Game"}
+              size={"medium"}
+              textColor={`$backgroundDark`}
+            ></TitleComponent>
+          </TitleContainer>
+          <ContainerGameDark>
+            <BackArrowContainer>
+              <BackArrowComponent actionOnClick={gotoMainPage} />
+            </BackArrowContainer>
+            <TitleComponent
+              text={"Create Game"}
+              size={"medium"}
+            ></TitleComponent>
+            <FormComponent
+              name={{ name, setName }}
+              creator={{ creator, setCreator }}
+              difficulty={{ difficulty, setDifficulty }}
+              onSubmit={actionOnSubmit}
+            ></FormComponent>
+          </ContainerGameDark>
         </PageContainer>
       );
     }
