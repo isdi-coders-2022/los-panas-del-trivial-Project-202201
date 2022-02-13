62df4875325f5151180a46a8faf814b179f9c9f7
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-02-13 11:38:14 +0100

    declare action and render ArrowPagesComponent

--- src/components/ArrowPagesComponent/ArrowPagesComponent.test.js
+++ src/components/ArrowPagesComponent/ArrowPagesComponent.test.js
@@ -1,5 +1,12 @@
+import { render } from "@testing-library/react";
+import ArrowPagesComponent from "./ArrowPagesComponent";
+
 describe("Given a ArrowPagesComponent", () => {
   describe("When the button receives an actionOnClick", () => {
-    test("Then it should call the action", () => {});
+    test("Then it should call the action", () => {
+      const action = jest.fn();
+
+      render(<ArrowPagesComponent actionOnClick={action} />);
+    });
   });
 });
